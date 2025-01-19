3e0ca2d2de8f0bd1e4048ebf779976ef274dc331
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2025-01-19 21:37:42 +0530

    MVC project

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.use((req, res, next) => {
 
 app.use('/listings', listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
-app.use("/", userRouter);
+app.use("/", listingsRouter);
 
 
 // app.get('/listings', async (req, res) => {
